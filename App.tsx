96e529b0e8061631c418060d1caf47dd6fa685f2
Anoopjadhav <anoopjadhav@gmail.com>
2022-09-27 18:58:09 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [todoInput, setTodoInputValue] = React.useState('');
+
+  const addTodoItem = () => {
+    setTodos([...todos, todoInput]);
+    setTodoInputValue('');
+  };
+
+  const clearTodoList = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul className="todo-items">
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+      <textarea
+        value={todoInput}
+        onChange={(e) => setTodoInputValue(e.target.value)}
+      />
+      <div className="buttons-wrapper">
+        <button type="reset" onClick={clearTodoList}>
+          Clear
+        </button>
+        <button onClick={addTodoItem}>Add</button>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# snapshot-testing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vqe4dr)
